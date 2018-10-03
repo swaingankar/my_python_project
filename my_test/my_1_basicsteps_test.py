afb6415f0a253d142dd88df280e4a8f1d7e05506
swaingankar <swaingankar@gmail.com>
2018-10-03 15:39:11 +0530

    Latest data

---
+++ my_test/my_1_basicsteps_test.py
@@ -0,0 +1,23 @@
+import pytest
+
+def test_success():
+    """this test succeeds"""
+    assert True
+
+def test_new_success():
+    """this test succeeds"""
+    assert True
+
+
+def test_failure():
+    """this test fails"""
+    assert True
+
+
+def test_skip():
+    """this test is skipped"""
+    pytest.skip('for a reason!')
+
+
+def test_broken():
+    raise Exception('oops again')
