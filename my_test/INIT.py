b5043df41e2a2e133a69aa7a4dc9af99db6d1be4
swaingankar <swaingankar@gmail.com>
2018-10-05 14:02:07 +0530

    added conditions

--- my_test/INIT.py
+++ my_test/INIT.py
@@ -3,9 +3,9 @@ import os
 import sys
 try:
 	files =  os.listdir("c:/Users/Sachin/.jenkins/workspace/my_jenkins_pytest/allure-report/history")
-	print(files)
+	#print(files)
 	for f in files:
-		print(f)
+		#print(f)
 		shutil.copy("c:/Users/Sachin/.jenkins/workspace/my_jenkins_pytest/allure-report/history/"+f,"c:/Users/Sachin/.jenkins/workspace/my_jenkins_pytest/allure-results/history",follow_symlinks=True)
-except:
-	print("Error message"+Exception)	
+except Exception as e:
+	print("Error message : " + format(e))	
