2bebcd1e09f6940d06d26caaf8f2728c7267ea4d
swaingankar <swaingankar@gmail.com>
2018-10-05 13:55:07 +0530

    init file added for history

---
+++ my_test/test_init.py
@@ -0,0 +1,11 @@
+import shutil
+import os
+import sys
+try:
+	files =  os.listdir("c:/Users/Sachin/.jenkins/workspace/my_jenkins_pytest/allure-report/history")
+	print(files)
+	for f in files:
+		print(f)
+		shutil.copy("c:/Users/Sachin/.jenkins/workspace/my_jenkins_pytest/allure-report/history/"+f,"c:/Users/Sachin/.jenkins/workspace/my_jenkins_pytest/allure-results/history",follow_symlinks=True)
+except:
+	print("Error message"+Exception)	
